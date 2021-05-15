feb10fb34072c9849fe13b415b62e8371d56d2d0
LeBaron <lebaron@3c22fb41074a.ant.amazon.com>
2021-05-14 22:01:43 -0700

    finished module 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About Me">
+        <p>Hi, I'm the proud creator of this site which I built with Gatsby.</p>
+      </Layout>
+    </main>
+  );
+};
+
+export default AboutPage;
